40856b5c537b708ebf17342a5ec5dd1f16479cc9
lvj1cob <v.balaji3@in.bosch.com>
2025-08-08 12:42:47 +0530

    fix:exmpy list testcase

--- stats.py
+++ stats.py
@@ -1,4 +1,26 @@
+"""
+stats.py - Module for calculating statistics
+like average, max, and min from a list of numbers.
+"""
+import math
+
+
+def list_is_empty(numbers):
+    """Check if the input list is empty."""
+    return len(numbers) == 0
+
 
 def calculateStats(numbers):
-  # implement the computation of statistics here and return the results
-  return None
+    """Calculate average, max, and min of a list of numbers."""
+    computedStats = {
+        "avg": math.nan,
+        "max": math.nan,
+        "min": math.nan
+    }
+    if list_is_empty(numbers):
+        return computedStats
+
+    computedStats["avg"] = math.fsum(numbers) / len(numbers)
+    computedStats["max"] = max(numbers)
+    computedStats["min"] = min(numbers)
+    return computedStats
